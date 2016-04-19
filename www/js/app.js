6c5228ab94b2b5fa84ba930403b11fba0ef4b92f
timothyra <contact@timraiser.com>
2016-04-19 21:53:05 +0200

    Changed Side Menu

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('PartnerTimeApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,5 +20,17 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+
+    // Functionality of the sibar menu
+    function ContentController($scope, $ionicSideMenuDelegate) {
+    $scope.toggleRight = function() {
+      $ionicSideMenuDelegate.toggleRight();
+    };
+  }
+
   });
-})
+
+
+
+});
